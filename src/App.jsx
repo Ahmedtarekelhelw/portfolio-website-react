9b8fd59fd83d237cfcbfb9a7d24fbec1f72985f7
Ahmedtarekelhelw <elhelwahmed4@gmail.com>
2022-01-15 13:44:40 +0200

    fix some issue with deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import "./App.scss";
 import Topbar from "./components/topbar/Topbar";
 
 import Sidebar from "./components/sidebar/Sidebar";
-// import Intro from "./components/intro/Intro";
-// import Portfolio from "./components/portfolio/Portfolio";
-// import Work from "./components/work/Work";
-// import Testimonials from "./components/testimonials/Testimonials";
-// import Contact from "./components/contact/Contact";
 import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./components/Index";
@@ -14,18 +9,17 @@ import Index from "./components/Index";
 function App() {
   const [active, setActive] = useState(false);
   return (
-    // <BrowserRouter>
-    <div className="App">
-      <Topbar setActive={(bool) => setActive(bool)} active={active} />
-      <Sidebar setActive={(bool) => setActive(bool)} active={active} />
-      <div className="sections">
-        {/* <Routes> */}
-        {/* <Route path="/portfolio-website-react" element={<Index />} /> */}
-        <Index />
-        {/* </Routes> */}
+    <BrowserRouter>
+      <div className="App">
+        <Topbar setActive={(bool) => setActive(bool)} active={active} />
+        <Sidebar setActive={(bool) => setActive(bool)} active={active} />
+        <div className="sections">
+          <Routes>
+            <Route path="/portfolio-website-react/" element={<Index />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-    // </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
